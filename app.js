0c855b90f90b6873e9a6014a766bc9dcb6ad7def
athenasbane <beedge@Benjamins-MacBook-Pro.local>
2020-06-27 14:32:24 +0100

    adding process.env for mongo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const schedule = require('node-schedule')
 
-const db = require(process.env.MongoURI)
+const db = process.env.MongoURI
 const Job = require('./src/models/Job')
 const scraper = require('./src/datascraper/dataScraper')
 
