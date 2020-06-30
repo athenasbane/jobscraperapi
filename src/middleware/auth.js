f00dd5b252ad378255bc453b203c6dc37e551a8d
athenasbane <beedge@Benjamins-MacBook-Pro.local>
2020-06-30 18:59:12 +0100

    keys error fix v4

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
-const jwtSecret = require('../../config/keys').JWT_SECRET
+const jwtSecret = process.env.JWT_SECRET
 
 
 const auth = async (req, res, next) => {
